17a8937421b4d78f0bbe620d12ecb0ed8dbc01c0
nishinoshungo <nishishun50@gmail.com>
2022-01-06 15:40:26 +0900

    study

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,22 +1,9 @@
-# def to_hex(r, g, b)
-#   '#' +
-#     r.to_s(16).rjust(2, '0') +
-#     g.to_s(16).rjust(2, '0') +
-#     b.to_s(16).rjust(2, '0')
-# end
-
-# def to_hex(r, g, b)
-#   hex = '#'
-#   [r, g, b].each do |n|
-#     hex += n.to_s(16).rjust(2, '0')
-#   end
-#   hex
-# end
-
 def to_hex(r, g, b)
   [r, g, b].inject('#') do |hex, n|
     hex + n.to_s(16).rjust(2, '0')
   end
 end
 
-p to_hex(55, 255, 0)
+def to_ints(hex)
+  hex.scan(/\w\w/).map(&:hex)
+end
